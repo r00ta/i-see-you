f0021a44285c08c40c83ea9c11e299ff34706435
r00ta <jacopo.r00ta@gmail.com>
2020-01-11 16:44:24 +0000

    Fix setup and requirements

--- setup/raspberrypi3/setup.sh
+++ setup/raspberrypi3/setup.sh
@@ -1,4 +1,8 @@
-sudo apt-get install git vim python3 python3-dev python3-pip python3-virtualenv
-sudo pip install virtualenv 
-sudo pip --update pip
+sudo apt-get install git libatlas-base-dev vim python3 python3-dev python3-pip python3-virtualenv
+sudo apt-get install -y libjasper-dev
+sudo apt-get install -y libqtgui4
+sudo apt-get install -y python3-pyqt5
+sudo apt-get install -y python3-opencv
+sudo apt-get install -y libqt4-test
+sudo pip3 install virtualenv 
 
